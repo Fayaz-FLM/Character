fac08d507fc133721de003213c20e1ddfcdc3d20
Fayaz <fayaz.sd@frontlinesedutech.com>
2025-02-11 20:06:14 +0530

    First Commit in Master

---
+++ CharacterFrequency/src/CountCharcaters.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class CountCharcaters {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String input;
+		Map<Character,Integer> charcatersMap = new HashMap<>();
+		
+		System.out.println("Enter String to count characters frequency : ");
+		input = sc.nextLine();
+		
+		for(int i=0; i<input.length();i++) {
+			char c = input.charAt(i);
+			charcatersMap.put(c, charcatersMap.getOrDefault(c, 0)+1);
+		}
+		
+		System.out.println(charcatersMap);
+		
+		sc.close();
+	}
+	
+
+}
